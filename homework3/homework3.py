ec878c61ebc2b30a052c9bdbb6d719dd083064d2
Artem Petrenko <109694317+LiarLiarr@users.noreply.github.com>
2022-07-28 00:07:56 +0300

    Add files via upload

---
+++ homework3/homework3.py
@@ -0,0 +1,10 @@
+dialoge = input("Привіт, мене звати PyCharm! Як тебе звати?")
+print(dialoge + ",вітаю")
+dialoge2 = input("Загадай число:")
+dialoge3 = input("Загадай інше число:")
+number1 = int(dialoge3)
+print(number1 + 4)
+
+
+
+
