2bbbb317c79a8ec2a8253637840909b0f565f363
Artem Petrenko <109694317+LiarLiarr@users.noreply.github.com>
2022-07-31 20:17:32 +0300

    Update Ex2.py

--- homework_4/Ex2.py
+++ homework_4/Ex2.py
@@ -1,8 +1,14 @@
 class1 = 19
-class2 = 15
-class3 = 17
-number1 = (((class3 + class2 + class1) % 2 * 2))
-number2 = ((class3 + class2 + class1) // 2)
-amount_of_desk = (number1 + number2)
+class2 = 17
+class3 = 15
+number1 = (class1 // 2)
+number2 = (class2 // 2)
+number3 = (class3 // 2)
+amount1 = number1 + (class1 % 2)
+amount2 = number2 + (class2 % 2)
+amount3 = number3 + (class3 % 2)
+amount_of_desk = (amount1 + amount2 + amount3)
 print(amount_of_desk)
-
+# print(amount1)
+# print(amount2)
+# print(amount3)
