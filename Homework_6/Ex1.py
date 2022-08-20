c25411617a28cb97bb4f0f31160361af37ab5ca0
Artem Petrenko <109694317+LiarLiarr@users.noreply.github.com>
2022-08-20 12:16:40 +0300

    Add files via upload

--- Homework_6/Ex1.py
+++ Homework_6/Ex1.py
@@ -1,9 +1,6 @@
-a = input()
-b = input()
-c = a + " " + b
-while a.isalpha() is not True or b.isalpha() is not True:
-    print("No")
-    break
-if a.isalpha() is True and b.isalpha() is True:
-    print(c[::-1].title())
+a, b = input("Введіть два лова через пробіл: ").split(" ")
 
+while a.isalpha() is not True or b.isalpha() is not True:
+    a, b = input("Введіть два лова через пробіл: ").split(" ")
+if a.isalpha() is True and b.isalpha() is True:
+    print(a[::-1].title(), b[::-1].title())
